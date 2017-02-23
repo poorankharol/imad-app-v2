6a3c8b5e300b84c7ceafd99153889dc856c0f471
poorankharol <kharolpooran@gmail.com>
2017-02-23 21:30:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,42 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+  title:'Article ome',
+  heading:'article one',
+  date:'sept 5',
+  content:`<p> This is ContentThis is ContentThis is ContentThis is ContentThis is Content</p>
+             <p>This is ContentThis is ContentThis is ContentThis is ContentThis is Content</p>`
+  
+};
+function html1(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+var html=`
+    <html>
+    <head>
+        <title> ${title}</title>
+        <meta name="viewport" content="width-device-width ,initial-scale=1"/> 
+    </head>
+    <body>
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <h3> ${heading} </h3>
+        <h3> ${date} </h3>
+        ${content}
+    </body>
+    </html>
+`;
+return html;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(html1(articleone));
 });
 app.get('/article-two',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
